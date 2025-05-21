6de17f0306baa1ffab52d7d7cde654a4321b6a7c
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-05-21 15:20:37 -0300

    Aula de Seletores Especiais no estio css( :host; :host-context(); ::ng-deep

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NewComponent],
+  styles:[`
+        h1 {
+          color:red;
+        }
+    `],
+  template: ` 
+  <router-outlet />
+
+  <h1>Curso de Angular</h1>
+  <div class="theme-red">
+      <app-new-component /> 
+  </div>
+   
+`,
+})
+export class AppComponent {}
