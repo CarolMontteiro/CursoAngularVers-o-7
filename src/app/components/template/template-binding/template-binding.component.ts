4f895c73b28ed1d26b65539f0675345af038703a
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-05-22 17:08:46 -0300

    Aula de Event binding

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -13,7 +13,28 @@ export class TemplateBindingComponent {
   public isDisable = true;
   public srcValue = 
   'https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/privadas-antigas/foto-oficial/fotos/45774283825_8673b82f64_m.jpg/@@images/image.jpeg';
-  public isTextDecoration = this.age >= 32 ? 'underline' : 'none'
+  public isTextDecoration = this.age >= 32 ? 'underline' : 'none';
 
-  public condition = this.age > 1 ? 'teste' : 'teste2'
+  public condition = this.age > 1 ? 'teste' : 'teste2';
+
+  public sum() {
+    return this.age++;
+  }
+
+  public sub() {
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event); 
+  }
+
+  public onMouseMove(event: MouseEvent) {
+    return console.log({
+      clietX: event.clientX,
+      clietyY: event.clientY,
+    });
+    
+  }
+
 }
