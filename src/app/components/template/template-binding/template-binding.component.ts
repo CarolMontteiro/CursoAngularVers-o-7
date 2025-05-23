a4dfe017460d42dab0b3f96501567af7738d57d2
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-05-23 15:34:33 -0300

    Aula de NgClass e NgStyle

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -1,16 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
   public name = 'Dener Troquatte';
-  public age = 30;
+  public age = 32;
   public isDisable = true;
   public srcValue = 
   'https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/privadas-antigas/foto-oficial/fotos/45774283825_8673b82f64_m.jpg/@@images/image.jpeg';
