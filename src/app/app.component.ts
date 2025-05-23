cd7d234cab3d5ab28866840414c0bc62b7c45e65
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-05-23 16:20:51 -0300

    Aula de Template variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { CommonModule } from '@angular/common';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
   template: ` 
-  // <router-outlet />
-
+  
   <h1>Curso de Angular</h1>
-  
-  
-  <app-new-component /> 
-  <app-template-binding />
+  <app-template-variables />
 `,
 })
 export class AppComponent {}
+
+
+// <router-outlet />
+// <app-new-component /> 
+// <app-template-binding />
