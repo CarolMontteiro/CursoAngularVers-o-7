07e0fd01e10df124a74c5060b8b3f249ed4b511b
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-05-26 19:50:46 -0300

    Aula de Control flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,17 @@ import { NewComponent } from './components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { CommonModule } from '@angular/common';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
   template: ` 
   
   <h1>Curso de Angular</h1>
-  <app-template-variables />
+  
+  <app-template-control-flow />
 `,
 })
 export class AppComponent {}
